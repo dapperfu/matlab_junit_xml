cca4010d762debade00e88642e4d2a649ebc7b96
Frey, Jed <jed-frey@users.noreply.github.com>
2017-07-30 23:30:24 -0400

    Development Sprint Auto Commit: Sun, Jul 30, 2017 11:30:24 PM

--- +junit/@TestSuite/TestSuite.m
+++ +junit/@TestSuite/TestSuite.m
@@ -18,6 +18,16 @@ classdef TestSuite < handle
     end
     
     methods
+               
+        function node = xml(self, docNode)
+            node = docNode.createElement('testsuite');
+            node.setAttribute('name', self.name);
+            for idx = 1:numel(self.test_cases)
+               test_case = self.test_cases(idx);
+               test_case_node = test_case.xml(docNode);
+               node.appendChild(test_case_node);
+            end
+        end
     end
     
 end
--- junit_xml_runner.m
+++ junit_xml_runner.m
@@ -1,9 +1,10 @@
 tc = junit.TestCase;
 tc.classname = 'some.class.name';
 tc.name = 'Test1';
-tc.time = '123.345';
+tc.elapsed_sec = '123.345';
 tc.stdout = 'I am stdout!';
 tc.stderr = 'I am stderr!';
 
 ts = junit.TestSuite;
 ts.name = 'my test suite';
+ts.test_cases=[tc];
