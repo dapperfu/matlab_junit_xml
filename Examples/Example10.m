dd33e52f2ae6522a9c314e4bcda37265da36ad3c
Frey, Jed <jed-frey@users.noreply.github.com>
2017-08-03 02:53:37 -0400

    Development Sprint Auto Commit: Thu, Aug  3, 2017  2:53:37 AM

--- Examples/Example10.m
+++ Examples/Example10.m
@@ -1,31 +1,43 @@
 %% Example 10
-% 'Real World' use case.
+% Full 'real world' example with passing, skipping & errors.
 
+% Create a test suite based on the filename.
 test_suite = junit.TestSuite(sprintf('%s Test Suite', mfilename));
 
-for random_integer = randi([0, 100], 100, 1)
+% Number of test cases to create.
+n_test_cases = 100;
+
+% Random integer: [0, 100].
+for random_integer = randi([0, 100], n_test_cases, 1)
+    % Create a test case.
     test_case = junit.TestCase(sprintf('Test Case #%d', random_integer));
-    
-    
     try
         % Complete failure, just skip the test.
-    
-        % Test Failure,
         assert(random_integer<10, 'Catastrophic Failure %d<10. Skipping Test', random_integer);
         try
+            % Test failed to run.
+            assert(random_integer<20, 'Test Failed: %d<10.', random_integer);
             try
-                
-            catch Error
+                % Test had an error during run.
+                assert(random_integer<40, 'Error runnining the test: %d<40', random_integer);
+                % 
+                test_case.stdout=sprintf('Test Success! %d', random_integer);
+            catch Error                
+                % Test had an error during execution.
                 test_case.error(Error.identifier, Error.message);
+                test_case.stderr=Error.message;
             end
         catch Fail
-            test_case.fail(Fail.identifier, Fail.message);     
+            % Test failed to run.
+            test_case.fail(Fail.identifier, Fail.message);
         end
     catch Skip
+        % Test was skipped.
         test_case.skipped(Skip.identifier, Skip.message)
     end
+    % Add the current test case to the test suite's test cases.
     test_suite.append(test_case);
     % test_suite.test_cases = [test_suite.test_cases, test_case];
 end
 % Write the TestCase as an xml file with .m filename.
-tc.xmlwrite(mfilename);
+test_suite.xmlwrite(mfilename);
